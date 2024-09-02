da5fe70184b6ade3dd3895345b8ba1951d110cbb
Biswajit Dutta <biswajitdutta664@gmail.com>
2024-09-02 14:16:44 +0530

    api key non env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,7 +5,8 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  // apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyCzLPf24NUuzjl7doOC6GeB8YVn8gFNzmM",
   authDomain: "reactchatapllication.firebaseapp.com",
   projectId: "reactchatapllication",
   storageBucket: "reactchatapllication.appspot.com",
@@ -16,6 +17,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage()
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
