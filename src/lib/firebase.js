3743f489a256caeba68aa58d200963b45d036b49
Biswajit Dutta <biswajitdutta664@gmail.com>
2024-09-02 10:04:40 +0530

    first commit

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchatapllication.firebaseapp.com",
+  projectId: "reactchatapllication",
+  storageBucket: "reactchatapllication.appspot.com",
+  messagingSenderId: "696510496914",
+  appId: "1:696510496914:web:8716d533ebffeec0c4e64b",
+  measurementId: "G-NQY4GD9564",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
